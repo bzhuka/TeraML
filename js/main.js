012e4c9819d8e73f469b3cb439d6889147e6d00d
OneRaynyDay <peifeng2005@gmail.com>
2015-11-21 13:45:02 -0800

    image and ajax commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,48 @@
+//Overall WS Access Variables
+dbsAlias = 'HackSys'
+wsHost = '153.64.73.150'
+wsPort =  '1080'
+path = '/tdrest/systems/' + dbsAlias + '/queries'
+username = "hack_user12"
+password = "tdhackathon"
 $(function(){
-	$("#diagnosis_button").click(function(){
-		$.ajax({
-		url: 'http://localhost:8080/SpringMVC/diagnoseUser',
-		type: 'POST',
-                dataType: 'json',
-                crossDomain: true,
-                data: 
-                	{"val" : $("#diagnosis_input").val()}
-        ,
-        success: function (json) {
-        	$("#output").append(JSON.stringify(json))
-        },            
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-        	alert("Was not able to process request : " + errorThrown);
-        }
-	});
-	})
+	$("#PCA_button").click(function(){
+                data = {"query" : 'select * from crime_data.murders_by_weapon_type'};
+                alert(JSON.stringify(data));
+		//--------this piece of code is to load into the mongodb once------
+                $.ajax({
+                        type: "POST",
+                        url: "http://"+wsHost+":"+wsPort+path,
+                        contentType: "application/json",
+                        headers: {
+                                'Accept': "application/vnd.com.teradata.rest-v1.0+json",
+                                'Authorization': 'Basic ' + btoa(username + ':' + password)
+                        },
+                        data: JSON.stringify({
+                                query: $("#PCA_input").val(),//"select * from crime_data.murders_by_weapon_type",
+                                format: 'object'
+                        }),
+                        success: function (json) {
+                                $("#PCA_output").append(JSON.stringify(json))
+                        },            
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                                alert("Was not able to process request : " + errorThrown);
+                        }
+                });
+                //----------
+                //--------------This piece of code is to query the python backend---------
+                /*$.ajax({
+                        type: "GET",
+                        url: "/query?val="+$("#PCA_input").val(),
+                        contentType: "application/json",
+                        data: {},
+                        success: function (json) {
+                                $("#PCA_output").append(JSON.stringify(json))
+                        },            
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                                alert("Was not able to process request : " + errorThrown);
+                        }
+                })*/
+        })
 })
+        
